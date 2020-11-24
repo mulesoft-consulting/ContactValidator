0f9854ba94e5465fc0a2e3e79a34d9ddc6b76a84
Adam Davis <adam.davis@mulesoft.com>
2020-11-24 17:28:18 +0000

    fixed test

--- src/test/java/org/mule/extension/contact/validator/ContactValidatorOperationsTestCase.java
+++ src/test/java/org/mule/extension/contact/validator/ContactValidatorOperationsTestCase.java
@@ -1,27 +1,33 @@
 package org.mule.extension.contact.validator;
 
+import org.junit.Test;
+import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import org.mule.functional.junit4.MuleArtifactFunctionalTestCase;
-import org.junit.Test;
 
 public class ContactValidatorOperationsTestCase extends MuleArtifactFunctionalTestCase {
 
-  /**
-   * Specifies the mule config xml with the flows that are going to be executed in the tests, this file lives in the test resources.
-   */
-  @Override
-  protected String getConfigFile() {
-    return "test-mule-config.xml";
-  }
+    /**
+     * Specifies the mule config xml with the flows that are going to be executed in the tests, this file lives in the test resources.
+     */
+    @Override
+    protected String getConfigFile() {
+        return "test-mule-config.xml";
+    }
 
-  @Test
-  public void executeSayHiOperation() throws Exception {
+    @Test
+    public void executeSayHiOperation() throws Exception {
+
+        Exception ex = null;
+        try {
+            String payloadValue = ((String) flowRunner("is-string-a-number-flow").run().getMessage().getPayload().getValue());
+        } catch (Exception e) {
+            ex = e;
+        }
 
 
-  String payloadValue = ((String) flowRunner("is-string-a-number-flow").run().getMessage().getPayload().getValue());
-
-    //assertThat(payloadValue, is("Hello Mariano Gonzalez!!!"));
-  }
+        assertThat(ex != null ? ex.getMessage() : "", is("Test for fail."));
+    }
 
 }
